aaf971b324ed61301d255db12a8a2b6c70dd5b82
Mark1626 <nimalan.m@protonmail.com>
2021-07-29 14:30:31 +0530

    Add option for parallel builds in make, perf and create dev image

--- genie.sh
+++ genie.sh
@@ -5,11 +5,19 @@ set -e
 print_usage() {
   echo "Usage: $0 [options]"
   echo
+  echo "Build Image"
+  echo
   echo "Options: "
-  echo "-m                             Mount yandasoft"
   echo "-b                             Build Images"
   echo "-v <version>                   Version of Image to Mount/Build"
-  echo "-i <image>                     Name of the Image (for mount only)"
+  echo "-t <num-threads>               Number of threads to use (default: 4)"
+  echo
+  echo "Mount Image"
+  echo
+  echo "Options: "
+  echo "-m                             Mount image on yandasoft folder"
+  echo "-i <image>                     Name of the Image"
+  echo "-d                             Debug Mode (enables perf)"
 }
 
 version=1.0
@@ -17,26 +25,41 @@ dockerfiledir="$PWD"/deploy/docker/custom
 yandasoftdir="$PWD"
 build=false
 mount=false
-mount_image="003-casarest"
+debug=false
+# mount_image="003-casarest"
+mount_image="004-yandasoft-dev-base"
+base_image="ubuntu:bionic"
+num_threads="4"
 
 build_base() {
-  m4 $dockerfiledir/001-yandabase | docker build -t 001-yandabase:$version -
+  m4 -Dbaseimage=$base_image $dockerfiledir/001-yandabase | docker build -t 001-yandabase:$version -
 }
 
 build_mpi() {
-  m4 -Dlatest=$version $dockerfiledir/001b-mpich-yandabase | docker build -t 001b-mpich-yandabase:$version -
+  m4 -DNUM_THREADS=$num_threads -Dlatest=$version $dockerfiledir/001b-mpich-yandabase | docker build -t 001b-mpich-yandabase:$version -
 }
 
 build_casa() {
-  m4 -Dlatest=$version $dockerfiledir/002-casacore | docker build -t 002-casacore:$version -
-  m4 -Dlatest=$version $dockerfiledir/003-casarest | docker build -t 003-casarest:$version -
+  m4 -DNUM_THREADS=$num_threads -Dlatest=$version $dockerfiledir/002-casacore | docker build -t 002-casacore:$version -
+  m4 -DNUM_THREADS=$num_threads -Dlatest=$version $dockerfiledir/003-casarest | docker build -t 003-casarest:$version -
 }
 
-build_slim_base() {
-  m4 -Dlatest=$version $dockerfiledir/alpine-base | docker build -t alpine-base:$version -
+build_dev_base() {
+  m4 -Dlatest=$version $dockerfiledir/004-yandasoft-dev-base | docker build -t 004-yandasoft-dev-base:$version -
 }
 
 mount_yandasoft() {
+  if $debug ; then
+    if [ -e $dockerfiledir/seccomp-perf.json ] ; then
+      docker run --rm -it -v $yandasoftdir:/home/yanda --security-opt seccomp=$dockerfiledir/seccomp-perf.json $mount_image:$version
+    else
+      echo 'Cannot enable perf_event_open seccomp-perf.json does not exist'
+      echo "Create a seccomp-perf.json in $dockerfiledir allowing perf_event_open"
+      echo 'Please refer to https://docs.docker.com/engine/security/seccomp/'
+      exit 1
+    fi
+  fi
+
   docker run --rm -it -v $yandasoftdir:/home/yanda $mount_image:$version
 }
 
@@ -45,7 +68,7 @@ if [ $# -eq 0 ]; then
   exit 0
 fi
 
-while getopts "bmv:i:h?" opt
+while getopts "bmdv:i:t:h?" opt
 do
   case "$opt" in
   [h?])
@@ -55,11 +78,17 @@ do
   b)
     build=true
     ;;
+  d)
+    debug=true
+    ;;
   m)
     mount=true
     ;;
   i)
     mount_image="$OPTARG"
+    ;;
+  t)
+    num_threads="$OPTARG"
     ;;
   v)
     version="$OPTARG"
@@ -75,7 +104,7 @@ if $build ; then
   build_base
   build_mpi
   build_casa
-  build_slim_base
+  build_dev_base
 fi
 
 if $mount ; then
